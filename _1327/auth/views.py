1a1ae0e1c4b87bac7f6260dc4bd93fc924bf3c1d
Johannes Wolf <janno42@gmail.com>
2014-07-29 10:39:14 +0200

    changed forms import

--- _1327/auth/views.py
+++ _1327/auth/views.py
@@ -3,17 +3,17 @@ from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.utils.translation import ugettext_lazy as _
-import forms
+from _1327.auth.forms import LoginForm
 
 def login(request):
 	if request.method == 'POST':
-		form = forms.LoginForm(request.POST)
+		form = LoginForm(request.POST)
 		if form.is_valid():
 			auth_login(request, form.get_user())
 			messages.success(request, _("You have been successfully logged in."))
 			return redirect(settings.LOGIN_REDIRECT_URL)
 	else:
-		form = forms.LoginForm()
+		form = LoginForm()
 
 	return render(request, "login.html", {
 		'form': form
