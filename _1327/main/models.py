ff449814858c9fc9f11c4c2e46c51a99da72464e
janno42 <janno42@gmail.com>
2014-09-29 20:47:59 +0200

    fixes #28; return username if first_name or last_name don't exist

--- _1327/main/models.py
+++ _1327/main/models.py
@@ -47,10 +47,14 @@ class UserProfile(AbstractBaseUser):
 	objects = UserManager()
 
 	def get_full_name(self):
-		return self.first_name + ' ' + self.last_name
+		if self.first_name and self.last_name:
+			return self.first_name + ' ' + self.last_name
+		return self.username
 
 	def get_short_name(self):
-		return self.first_name
+		if self.first_name:
+			return self.first_name
+		return self.username
 
 	def __str__(self):
 		return self.get_full_name();
