810e7fa1d74e7d387ca2a64d495673352930b67d
Steffen Kötte <steffen.koette@gmail.com>
2015-02-16 22:41:29 +0100

    text of documents are autosaved every ten seconds

--- _1327/information_pages/views.py
+++ _1327/information_pages/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.translation import ugettext_lazy as _
 
 from _1327.user_management.decorators import admin_required
-from _1327.documents.utils import handle_edit, prepare_versions
+from _1327.documents.utils import handle_edit, handle_autosave, prepare_versions
 from _1327.documents.models import Document
 
 
@@ -23,6 +23,17 @@ def edit(request, title):
 			'form': form,
 			'active_page': 'edit',
 		})
+
+@admin_required
+def autosave(request, title):
+	document = None
+	try:
+		document = Document.objects.get(url_title=title, type='I')
+	except Document.DoesNotExist:
+		pass
+
+	handle_autosave(request, document)
+	return HttpResponse()
 
 @admin_required
 def versions(request, title):
