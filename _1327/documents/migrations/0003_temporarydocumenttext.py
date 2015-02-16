734fb9d3d0a7f8f0dd9925926149a0aa4a75bf80
Steffen Kötte <steffen.koette@gmail.com>
2015-02-16 22:42:14 +0100

    add timestamp to autosave, function to restore autosave, tests

--- _1327/documents/migrations/0003_temporarydocumenttext.py
+++ _1327/documents/migrations/0003_temporarydocumenttext.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
 
 
 class Migration(migrations.Migration):
@@ -17,6 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('text', models.TextField()),
                 ('document', models.ForeignKey(to='documents.Document', related_name='document')),
+                ('created', models.DateTimeField(default=datetime.datetime(2015, 2, 16, 19, 12, 14, 661384, tzinfo=utc), auto_now=True)),
             ],
             options={
             },
