a1ed97f9df434e3bf93e26f83a2deefc8086d385
Sebastian Brückner <sebbrueckner@gmail.com>
2015-05-18 22:59:46 +0200

    Fixed URL format.

--- _1327/documents/urls.py
+++ _1327/documents/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('_1327.documents.views',
-	url(r"revert/$", 'revert', name='revert'),
-	url(r"deleteattachment/$", 'delete_attachment', name='delete_attachment'),
-	url(r"download/$", 'download_attachment', name='download_attachment'),
+	url(r"revert$", 'revert', name='revert'),
+	url(r"attachment/delete$", 'delete_attachment', name='delete_attachment'),
+	url(r"attachment/download$", 'download_attachment', name='download_attachment'),
 )
--- _1327/minutes/urls.py
+++ _1327/minutes/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('_1327.minutes.views',
-	url(r"list/$", 'list', name='list'),
-	url(r"edit/(?P<title>[\w-]+)/$", 'edit', name='edit'),
-	url(r"autosave/(?P<title>[\w-]+)/$", 'autosave', name='autosave'),
-	url(r"versions/(?P<title>[\w-]+)/$", 'versions', name="versions"),
-	url(r"permissions/(?P<title>[\w-]+)/$", 'permissions', name="permissions"),
-	url(r"attachments/(?P<title>[\w-]+)/$", 'attachments', name="attachments"),
-	url(r"(?P<title>[\w-]+)/$", 'view', name='view'),
+	url(r"^$", 'list', name='list'),
+	url(r"(?P<title>[\w-]+)/edit$", 'edit', name='edit'),
+	url(r"(?P<title>[\w-]+)/autosave$", 'autosave', name='autosave'),
+	url(r"(?P<title>[\w-]+)/versions$", 'versions', name="versions"),
+	url(r"(?P<title>[\w-]+)/permissions$", 'permissions', name="permissions"),
+	url(r"(?P<title>[\w-]+)/attachments$", 'attachments', name="attachments"),
+	url(r"(?P<title>[\w-]+)$", 'view', name='view'),
 )
