f9503e09bc66e2811f5aa1e25ca0e7ae6f3820d0
Sebastian Brückner <sebbrueckner@gmail.com>
2015-07-13 18:23:57 +0200

    Fixed navigation context processor in case a view does not exist.

---
+++ _1327/main/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, RequestFactory
+from model_mommy import mommy
+
+from .context_processors import mark_selected
+from .models import MenuItem
+
+
+class TestMenuProcessor(TestCase):
+
+	def test_mark_selected(self):
+		rf = RequestFactory()
+		request = rf.get('/this_is_a_page_that_most_certainly_does_not_exist.html')
+
+		menu_item = mommy.make(MenuItem)
+		try:
+			mark_selected(request, menu_item)
+		except AttributeError:
+			self.fail("mark_selected() raises an AttributeError")
