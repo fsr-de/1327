f5392701beb783557af7b6dd5c156cd18e00fafe
Steffen Kötte <steffen.koette@gmail.com>
2016-02-08 23:19:38 +0100

    fix #227 add color to labels

--- _1327/minutes/models.py
+++ _1327/minutes/models.py
@@ -7,9 +7,27 @@ from reversion import revisions
 
 from _1327.documents.models import Document
 from _1327.user_management.models import UserProfile
+from .fields import HexColorModelField
 
 
 MINUTES_VIEW_PERMISSION_NAME = 'view_minutesdocument'
+
+
+class MinutesLabel(models.Model):
+	title = models.CharField(max_length=255)
+	color = HexColorModelField()
+
+	def __str__(self):
+		return self.title
+
+	@property
+	def class_for_text_color(self):
+		# Calculate contrast, see https://24ways.org/2010/calculating-color-contrast/
+		r = int(self.color[1:3], 16)
+		g = int(self.color[3:5], 16)
+		b = int(self.color[5:7], 16)
+		yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000
+		return 'dark-text' if (yiq >= 128) else 'bright-text'
 
 
 class MinutesDocument(Document):
@@ -26,6 +44,7 @@ class MinutesDocument(Document):
 	moderator = models.ForeignKey(UserProfile, related_name='moderations', verbose_name=_("Moderator"), blank=True, null=True)
 	author = models.ForeignKey(UserProfile, related_name='documents')
 	participants = models.ManyToManyField(UserProfile, related_name='participations', verbose_name=_("Participants"))
+	labels = models.ManyToManyField(MinutesLabel, related_name="minutes", blank=True)
 
 	VIEW_PERMISSION_NAME = MINUTES_VIEW_PERMISSION_NAME
 
@@ -47,14 +66,6 @@ class MinutesDocument(Document):
 		return user.has_perm(permission_name, self) or user.has_perm(permission_name)
 
 revisions.register(MinutesDocument, follow=["document_ptr"])
-
-
-class MinutesLabel(models.Model):
-	title = models.CharField(max_length=255)
-	minutes = models.ManyToManyField(MinutesDocument, related_name="labels", blank=True)
-
-	def __str__(self):
-		return self.title
 
 
 class Guest(models.Model):
