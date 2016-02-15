53f6ad98e27ecc3ea7a743e98ca3868ebb452019
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2016-02-15 18:24:27 +0100

    add tests for main page

--- _1327/main/tests.py
+++ _1327/main/tests.py
@@ -1,6 +1,10 @@
+from django.core.urlresolvers import reverse
 from django.test import RequestFactory, TestCase
+from django_webtest import WebTest
 from model_mommy import mommy
 
+from _1327.information_pages.models import InformationDocument
+from _1327.minutes.models import MinutesDocument
 from .context_processors import mark_selected
 from .models import MenuItem
 
@@ -16,3 +20,25 @@ class TestMenuProcessor(TestCase):
 			mark_selected(request, menu_item)
 		except AttributeError:
 			self.fail("mark_selected() raises an AttributeError")
+
+
+class MainPageTests(WebTest):
+
+	def test_main_page_no_page_set(self):
+		response = self.app.get(reverse('index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'index.html')
+
+	def test_main_page_information_page_set(self):
+		document = mommy.make(InformationDocument)
+		with self.settings(MAIN_PAGE_ID=document.id):
+			response = self.app.get(reverse('index'))
+			self.assertEqual(response.status_code, 200)
+			self.assertTemplateUsed(response, 'information_pages_base.html')
+
+	def test_main_page_minutes_document_set(self):
+		document = mommy.make(MinutesDocument)
+		with self.settings(MAIN_PAGE_ID=document.id):
+			response = self.app.get(reverse('index'))
+			self.assertEqual(response.status_code, 200)
+			self.assertTemplateUsed(response, 'minutes_base.html')
