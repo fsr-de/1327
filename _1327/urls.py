b51ec673f7349021510714f201afb08502683099
Johannes Wolf <janno42@gmail.com>
2016-03-07 21:03:12 +0100

    enable editing menu items outside of the admin area

--- _1327/urls.py
+++ _1327/urls.py
@@ -11,6 +11,11 @@ urlpatterns = patterns('_1327',  # noqa
 	url(r"^logout$", 'user_management.views.logout', name='logout'),
 	url(r'^polls/', include('_1327.polls.urls', namespace='polls')),
 
+	url(r'^menu_items$', 'main.views.menu_items_index', name='menu_items_index'),
+	url(r"^menu_item/create$", 'main.views.menu_item_create', name="menu_item_create"),
+	url(r"^menu_item/(\d+)/edit$", 'main.views.menu_item_edit', name="menu_item_edit"),
+	url(r"^menu_item/(\d+)/delete$", 'main.views.menu_item_delete', name="menu_item_delete"),
+
 	url(r'^admin/', include(admin.site.urls)),
 	url(r"^", include('_1327.information_pages.urls', namespace='information_pages')),
 )
