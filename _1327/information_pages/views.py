2e1802c2a22675bfc92a9fad6a184fedbc976e3a
Johannes Wolf <janno42@gmail.com>
2016-10-31 16:32:15 +0100

    show message when list of unlinked documents is empty

--- _1327/information_pages/views.py
+++ _1327/information_pages/views.py
@@ -7,8 +7,11 @@ from _1327.information_pages.models import InformationDocument
 
 # shows a list of information documents that are not accessible via a menu entry
 def unlinked_list(request):
-	permission_name = InformationDocument.objects.first().edit_permission_name  # the property can only be accessed from an instance of the class
-	information_pages = get_objects_for_user(request.user, permission_name, klass=InformationDocument.objects.filter(menu_items__isnull=True)).order_by('title')
+	if InformationDocument.objects.exists():
+		permission_name = InformationDocument.objects.first().edit_permission_name  # the property can only be accessed from an instance of the class
+		information_pages = get_objects_for_user(request.user, permission_name, klass=InformationDocument.objects.filter(menu_items__isnull=True)).order_by('title')
+	else:
+		information_pages = []
 
 	return render(request, "unlinked_list.html", {
 		'information_pages': information_pages,
