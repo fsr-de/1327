d29ecdfb67f0c12296de02ea6db4d9972b9b7724
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2017-02-20 19:38:37 +0100

    show string if internal link does is not present anymore

--- _1327/documents/markdown_internal_link_pattern.py
+++ _1327/documents/markdown_internal_link_pattern.py
@@ -1,13 +1,19 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import ugettext_lazy as _
+
 import markdown
 from markdown.inlinepatterns import LinkPattern
 
 
-class InternalLinkPattern (LinkPattern):
+class InternalLinkPattern(LinkPattern):
 
 	def handleMatch(self, m):
 		el = markdown.util.etree.Element("a")
-		el.set('href', self.url(m.group('id')))
-		el.text = markdown.util.AtomicString(m.group('title'))
+		try:
+			el.set('href', self.url(m.group('id')))
+			el.text = markdown.util.AtomicString(m.group('title'))
+		except ObjectDoesNotExist:
+			el.text = markdown.util.AtomicString(_('[missing link]'))
 		return el
 
 	def url(id):
