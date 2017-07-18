87f82252316097113f831121cdbee6c317dfafab
Johannes Wolf <janno42@gmail.com>
2017-07-18 21:35:18 +0200

    remove GROUPS_HIDDEN_DURING_CREATION and use specific group add permissions instead

--- _1327/polls/forms.py
+++ _1327/polls/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.conf import settings
 from django.contrib.auth.models import Group
-from django.core.exceptions import ValidationError
+from django.core.exceptions import PermissionDenied, ValidationError
 from django.forms import inlineformset_factory
 from django.utils.translation import ugettext_lazy as _
 
@@ -17,8 +17,31 @@ class PollForm(DocumentForm):
 		fields = ['title', 'url_title', 'text', 'start_date', 'end_date', 'max_allowed_number_of_answers', 'show_results_immediately', 'comment', 'group', 'vote_groups']
 
 	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
+		user = kwargs.pop('user', None)
 		creation = kwargs.pop('creation', None)
+		super().__init__(*args, **kwargs)
+		staff = Group.objects.get(name=settings.STAFF_GROUP_NAME)
+
+		if user.is_superuser:
+			permitted_groups = Group.objects.filter(permissions__codename="add_poll")
+		else:
+			permitted_groups = user.groups.filter(permissions__codename="add_poll")
+
+		if permitted_groups.count() == 0:  # The user should not be able to view this form
+			raise PermissionDenied
+
+		self.fields['group'].queryset = permitted_groups.all()
+		self.fields['group'].widget.attrs['class'] = 'select2-selection'
+		if creation:
+			if len(permitted_groups.all()) == 1:
+				self.fields['group'].initial = permitted_groups.first()
+				self.fields['group'].widget = forms.HiddenInput()
+			elif staff in permitted_groups.all() and not self.fields['group'].initial:
+				self.fields['group'].initial = staff
+		else:
+			self.fields['group'].widget = forms.HiddenInput()
+			self.fields['group'].required = False
+
 		if creation:
 			self.fields['vote_groups'].widget.attrs['class'] = 'select2-selection'
 		else:
