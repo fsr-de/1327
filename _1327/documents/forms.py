cd51cc7fe9b55d3940f9101067a7e02116a80354
Johannes Wolf <janno42@gmail.com>
2017-07-31 20:42:52 +0200

    refactoring

--- _1327/documents/forms.py
+++ _1327/documents/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.conf import settings
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import PermissionDenied
 from django.forms import BaseInlineFormSet
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
@@ -21,10 +22,31 @@ class DocumentForm(forms.ModelForm):
 		fields = ['title', 'text', 'url_title', 'group', 'comment']
 
 	def __init__(self, *args, **kwargs):
-		kwargs.pop('user', None)
-		kwargs.pop('creation', None)
+		user = kwargs.pop('user', None)
+		creation = kwargs.pop('creation', None)
 		kwargs.pop('creation_group', None)
+		staff = Group.objects.get(name=settings.STAFF_GROUP_NAME)
 		super().__init__(*args, **kwargs)
+
+		add_permission_name = self.Meta.model().add_permission_name.split('.')[1]
+		if user.is_superuser:
+			permitted_groups = Group.objects.filter(permissions__codename=add_permission_name)
+		else:
+			permitted_groups = user.groups.filter(permissions__codename=add_permission_name)
+			if creation and permitted_groups.count() == 0:  # The user should not be able to view this form
+				raise PermissionDenied
+
+		self.fields['group'].queryset = permitted_groups.all()
+		self.fields['group'].widget.attrs['class'] = 'select2-selection'
+		if creation:
+			if len(permitted_groups.all()) == 1:
+				self.fields['group'].initial = permitted_groups.first()
+				self.fields['group'].widget = forms.HiddenInput()
+			elif staff in permitted_groups.all() and not self.fields['group'].initial:
+				self.fields['group'].initial = staff
+		else:
+			self.fields['group'].widget = forms.HiddenInput()
+			self.fields['group'].required = False
 
 	def clean_url_title(self):
 		super().clean()
