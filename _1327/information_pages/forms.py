01f288484f65cce13838c2a99472d3a8d3fa6140
Johannes Wolf <janno42@gmail.com>
2017-07-31 20:42:52 +0200

    refactoring

--- _1327/information_pages/forms.py
+++ _1327/information_pages/forms.py
@@ -1,8 +1,3 @@
-from django import forms
-from django.conf import settings
-from django.contrib.auth.models import Group
-from django.core.exceptions import PermissionDenied
-
 from _1327.documents.forms import DocumentForm
 from .models import InformationDocument
 
@@ -11,32 +6,6 @@ class InformationDocumentForm(DocumentForm):
 	class Meta(DocumentForm.Meta):
 		model = InformationDocument
 		fields = ['title', 'text', 'url_title', 'is_menu_page', 'comment']
-
-	def __init__(self, *args, **kwargs):
-		user = kwargs.pop('user', None)
-		creation = kwargs.pop('creation', None)
-		kwargs.pop('creation_group', None)
-		super().__init__(*args, **kwargs)
-		staff = Group.objects.get(name=settings.STAFF_GROUP_NAME)
-
-		if user.is_superuser:
-			permitted_groups = Group.objects.filter(permissions__codename="add_informationdocument")
-		else:
-			permitted_groups = user.groups.filter(permissions__codename="add_informationdocument")
-			if creation and permitted_groups.count() == 0:  # The user should not be able to view this form
-				raise PermissionDenied
-
-		self.fields['group'].queryset = permitted_groups.all()
-		self.fields['group'].widget.attrs['class'] = 'select2-selection'
-		if creation:
-			if len(permitted_groups.all()) == 1:
-				self.fields['group'].initial = permitted_groups.first()
-				self.fields['group'].widget = forms.HiddenInput()
-			elif staff in permitted_groups.all() and not self.fields['group'].initial:
-				self.fields['group'].initial = staff
-		else:
-			self.fields['group'].widget = forms.HiddenInput()
-			self.fields['group'].required = False
 
 
 InformationDocument.Form = InformationDocumentForm
--- _1327/minutes/forms.py
+++ _1327/minutes/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.contrib.auth.models import Group
-from django.core.exceptions import PermissionDenied
 from django.forms import inlineformset_factory
 
 from _1327.documents.forms import DocumentForm
@@ -15,37 +13,18 @@ class MinutesDocumentForm(DocumentForm):
 		fields = ['title', 'date', 'moderator', 'author', 'participants', 'labels', 'state', 'text', 'comment', 'url_title', 'group']
 
 	def __init__(self, *args, **kwargs):
-		user = kwargs.pop('user', None)
-		creation = kwargs.pop('creation', None)
-		creation_group = kwargs.pop('creation_group', None)
+		user = kwargs.get('user', None)
+		creation = kwargs.get('creation', None)
+		creation_group = kwargs.get('creation_group', None)
 		super().__init__(*args, **kwargs)
 
-		if user.is_superuser:
-			permitted_groups = Group.objects.filter(permissions__codename="add_minutesdocument")
-		else:
-			permitted_groups = user.groups.filter(permissions__codename="add_minutesdocument")
-
-		if permitted_groups.count() == 0:  # The user should not be able to view this form
-			raise PermissionDenied
-
-		self.fields['group'].queryset = permitted_groups.all()
-		self.fields['group'].widget.attrs['class'] = 'select2-selection'
 		if creation and creation_group:
-			if len(permitted_groups.all()) == 1:
-				self.fields['group'].initial = permitted_groups.first()
-				self.fields['group'].widget = forms.HiddenInput()
-			elif not self.fields['group'].initial:
-				self.fields['group'].initial = creation_group
-
 			last_minutes_document = get_last_minutes_document_for_group(creation_group)
 			if last_minutes_document:
 				self.initial['moderator'] = last_minutes_document.moderator
 				self.initial['title'] = last_minutes_document.title
 			else:
 				self.initial['moderator'] = user
-		else:
-			self.fields['group'].widget = forms.HiddenInput()
-			self.fields['group'].required = False
 
 		if not self.instance.participants.exists() and creation_group:
 			self.initial['participants'] = [user.id for user in creation_group.user_set.all()]
