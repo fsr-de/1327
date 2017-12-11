6c9c5e4599332e844c3324ed8abcc3147c976176
Juliane Waack <jkwaack@gmail.com>
2017-12-11 20:17:09 +0100

    add search through Minutes option

--- _1327/minutes/views.py
+++ _1327/minutes/views.py
@@ -1,21 +1,25 @@
+import re
+
 from django.contrib.auth.models import Group
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import Http404, render
+from django.shortcuts import Http404, redirect, render
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 from guardian.core import ObjectPermissionChecker
 
+from _1327.minutes.forms import SearchForm
 from _1327.minutes.models import MinutesDocument
 
 
-def list(request, groupid):
+def get_permitted_minutes(minutes, request, groupid):
 	groupid = int(groupid)
 	try:
 		group = Group.objects.get(id=groupid)
 	except ObjectDoesNotExist:
 		raise Http404
-	result = {}
 
 	own_group = request.user.is_superuser or group in request.user.groups.all()
-	minutes = MinutesDocument.objects.all().prefetch_related('labels').order_by('-date')
+
 	# Prefetch group permissions
 	group_checker = ObjectPermissionChecker(group)
 	group_checker.prefetch_perms(minutes)
@@ -30,6 +34,7 @@ def list(request, groupid):
 		ip_range_group = Group.objects.get(name=ip_range_group_name)
 		ip_range_group_checker = ObjectPermissionChecker(ip_range_group)
 
+	permitted_minutes = []
 	for m in minutes:
 		# we show all documents for which the requested group has edit permissions
 		# e.g. if you request FSR minutes, all minutes for which the FSR group has edit rights will be shown
@@ -38,10 +43,68 @@ def list(request, groupid):
 		# we only show documents for which the user has view permissions
 		if not user_checker.has_perm(MinutesDocument.get_view_permission(), m) and (not ip_range_group_name or not ip_range_group_checker.has_perm(MinutesDocument.get_view_permission(), m)):
 			continue
+		permitted_minutes.append(m)
+
+	return permitted_minutes, own_group
+
+
+def search(request, groupid):
+	if request.method == 'POST':
+		form = SearchForm(request.POST)
+		if form.is_valid():
+			search_text = form.cleaned_data['search_phrase']
+	else:
+		# redirect to minutes list
+		return redirect("minutes:list", groupid=groupid)
+
+	# filter for documents that contain the searched for string
+	minutes = MinutesDocument.objects.filter(text__icontains=search_text).prefetch_related('labels').order_by('-date')
+
+	# only show permitted documents
+	minutes, own_group = get_permitted_minutes(minutes, request, groupid)
+
+	# find lines containing the searched for string
+	result = {}
+	for m in minutes:
+		# find lines with the searched for string and mark it as bold
+		lines = m.text.splitlines()
+		lines = [
+			mark_safe(
+				re.sub(
+					r'(' + re.escape(escape(search_text)) + ')',
+					r'<b>\1</b>', escape(line),
+					flags=re.IGNORECASE
+				)
+			)
+			for line in lines if (line.casefold().find(search_text.casefold()) != -1)
+		]
+
 		if m.date.year not in result:
 			result[m.date.year] = []
-		result[m.date.year].append(m)
+
+		result[m.date.year].append((m, lines))
+
+	return render(request, "minutes_with_lines_list.html", {
+		'minutes_list': sorted(result.items(), reverse=True),
+		'own_group': own_group,
+		'group_id': groupid,
+		'search_form': SearchForm(),
+		'phrase': search_text,
+	})
+
+
+def list(request, groupid):
+	minutes = MinutesDocument.objects.all().prefetch_related('labels').order_by('-date')
+	minutes, own_group = get_permitted_minutes(minutes, request, groupid)
+
+	result = {}
+	for m in minutes:
+		if m.date.year not in result:
+			result[m.date.year] = []
+		result[m.date.year].append((m, []))
 	return render(request, "minutes_list.html", {
 		'minutes_list': sorted(result.items(), reverse=True),
-		'own_group': own_group
+		'own_group': own_group,
+		'group_id': groupid,
+		'search_form': SearchForm(),
 	})
