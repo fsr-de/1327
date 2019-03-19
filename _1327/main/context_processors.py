e386b4bdd57b4b9a9d1c0da27c9ac6bf51ebe412
Johannes Wolf <johannes-wolf@posteo.de>
2019-03-19 21:07:27 +0100

    make logo and favicon path customizable

--- _1327/main/context_processors.py
+++ _1327/main/context_processors.py
@@ -75,3 +75,10 @@ def can_create_poll(request):
 
 def can_change_menu_items(request):
 	return {'CAN_CHANGE_MENU_ITEMS': request.user.is_superuser or len(get_objects_for_user(request.user, MenuItem.CHANGE_CHILDREN_PERMISSION_NAME, klass=MenuItem)) > 0}
+
+
+def image_paths(request):
+	return {
+		'LOGO_FILE': settings.LOGO_FILE,
+		'FAVICON_FILE': settings.FAVICON_FILE
+	}
