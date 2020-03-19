ad2f6e6dd7353cff900ce336788588f1ab3e06fe
Julian B <jeriox@users.noreply.github.com>
2020-03-19 19:32:41 +0100

    Lint

--- _1327/user_management/forms.py
+++ _1327/user_management/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group
 from django.forms import BooleanField, ModelMultipleChoiceField
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.debug import sensitive_variables
