44525c810665a507922da783c897b52b26be6f98
Paula <Paula.klinke@student.hpi.de>
2021-03-04 11:57:44 +0100

    Remove deprecation and fix sharing documents

--- _1327/routing.py
+++ _1327/routing.py
@@ -3,14 +3,16 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf import settings
 from django.urls import path
 from _1327.documents.consumers import PreviewConsumer
+from django.core.asgi import get_asgi_application
 
 
 websocket_urlpatterns = [
-	path("{preview_url}/<hash_value>".format(preview_url=settings.PREVIEW_URL.lstrip('/')), PreviewConsumer),
+	path("{preview_url}/<hash_value>".format(preview_url=settings.PREVIEW_URL.lstrip('/')), PreviewConsumer.as_asgi()),
 ]
 
 
 application = ProtocolTypeRouter({
+	'http': get_asgi_application(),
 	'websocket': AuthMiddlewareStack(
 		URLRouter(
 			websocket_urlpatterns
