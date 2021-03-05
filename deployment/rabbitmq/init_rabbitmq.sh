9c83ff514b2434661df5d93b801374802bba3eee
Paula <Paula.klinke@student.hpi.de>
2021-03-05 10:24:58 +0100

    Change travis.yml to working rabbitmq
    Co-authored-by: janno42 <janno42@users.noreply.github.com>

--- deployment/rabbitmq/init_rabbitmq.sh
+++ deployment/rabbitmq/init_rabbitmq.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
 # installed with https://computingforgeeks.com/how-to-install-latest-rabbitmq-server-on-ubuntu-linux/
-apt-get -q install -y rabbitmq-server
-
-#mkdir /opt/redis
-
-#cd /opt/redis
-# Use latest stable
-#wget -q http://download.redis.io/redis-stable.tar.gz
-# Only update newer files
-#tar -xz --keep-newer-files -f redis-stable.tar.gz
-
-#cd redis-stable
-#make
-#make install
-#mkdir -p /etc/redis
-#mkdir /var/lib/redis
-#chmod -R 770 /var/lib/redis
-#adduser --system --group --no-create-home redis
-#chown redis:redis /var/lib/redis
-
-# rabbitmq port 55672
-#cp -u /vagrant/deployment/redis/redis.conf /etc/redis/6379.conf
-#cp -u /vagrant/deployment/redis/redis.service /etc/systemd/system/redis.service
+sudo apt-get -q install -y rabbitmq-server
 
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
