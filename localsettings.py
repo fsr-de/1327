772d68b85a11170288f1ec2b9c798d6268aeb844
Julian B <jeriox@users.noreply.github.com>
2021-03-13 11:30:42 +0100

    fix my setup

--- localsettings.py
+++
@@ -1,7 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',   # postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                         # Or path to database file if using sqlite3.
-    }
-}
-DEBUG = True
