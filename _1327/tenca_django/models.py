e1426a7913842abd307c037d4231f5d44cf3cb59
Sven Koehler <sven.koehler@hpi.de>
2021-03-13 17:48:48 +0100

    Add tenca model, implement hash storage

--- _1327/tenca_django/models.py
+++ _1327/tenca_django/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from tenca.hash_storage import (HashStorage, MailmanDescriptionHashStorage,
+                                NotInStorageError, TwoLevelHashStorage)
 
-# Create your models here.
+
+class HashEntry(models.Model):
+	hash_id = models.CharField(max_length=64, unique=True, blank=False, null=False)
+	list_id = models.CharField(max_length=128, blank=False, null=False)
+
+
+class LegacyAdminURL(models.Model):
+	hash_id = models.ForeignKey(HashEntry,
+		related_name='legacy_admin_url',
+		on_delete=models.CASCADE
+	)
+	admin_url = models.CharField(max_length=32, blank=False, null=False)
+
+
+class DjangoModelHashStorage(HashStorage):
+
+	def __contains__(self, hash_id):
+		try:
+			HashEntry.objects.get(hash_id=hash_id)
+			return True
+		except HashEntry.DoesNotExist:
+			return False
+
+	def get_list_id(self, hash_id):
+		try:
+			entry = HashEntry.objects.get(hash_id=hash_id)
+		except HashEntry.DoesNotExist:
+			raise NotInStorageError()
+		else:
+			return entry.list_id
+
+	def store_list_id(self, hash_id, list_id):
+		entry = HashEntry(hash_id=hash_id, list_id=list_id)
+		entry.save()
+
+	def get_hash_id(self, list_id):
+		try:
+			entry = HashEntry.objects.get(list_id=list_id)
+		except HashEntry.DoesNotExist:
+			raise NotInStorageError()
+		else:
+			return entry.hash_id
+
+	def delete_hash_id(self, hash_id):
+		try:
+			entry = HashEntry.objects.get(hash_id=hash_id)
+		except HashEntry.DoesNotExist:
+			pass
+		else:
+			entry.delete()
+
+	def hashes(self):
+		return (e.hash_id for e in HashEntry.objects.all())
+
+
+class DjangoModelCachedDescriptionHashStorage(TwoLevelHashStorage):
+
+	l1_class = DjangoModelHashStorage
+	l2_class = MailmanDescriptionHashStorage
--- _1327/tenca_django/tests.py
+++ _1327/tenca_django/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+from tenca.tests import test_hash_storage
+from _1327.tenca_django.models import DjangoModelHashStorage
+
+class DjangoModelHashStorageTest(TestCase, test_hash_storage.HiddenFromTestRunner.HashStorageTest):
+	StorageClass = DjangoModelHashStorage
