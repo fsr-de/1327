52cd2b0b601750ce06866afc61263f502d0912f5
Julian B <jeriox@users.noreply.github.com>
2021-03-13 11:48:56 +0100

    add connection

--- _1327/tenca_django/views.py
+++ _1327/tenca_django/views.py
@@ -1,5 +1,7 @@
+from tenca import connection
 from django.views.generic import TemplateView
 
+connection = connection.Connection()
 
 class TencaDashboard(TemplateView):
 	template_name = "tenca_django/dashboard.html"
